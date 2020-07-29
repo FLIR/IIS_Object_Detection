8d4abfd3ce75ce15f13e8caa445b5f0b8a7576aa
Xu, Di <Di.Xu@flir.com>
2020-07-28 21:19:14 -0700

    added auto generate labelmap.prototxt file

--- template/run.sh
+++ template/run.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 . ../project.config
 
-## DATA
-# [2] Copy and modify labelmap.prototxt 
+## [1] DATA
+# [1a] Generate labelmap.prototxt file
 echo 'mkdir: ' data/${script_folder_name}
 mkdir -p data/${script_folder_name}
 FILE=data/${script_folder_name}/labelmap.prototxt
 if test -f "$FILE"; then
-    echo "$FILE exists."
-else 
-	echo "$FILE does not exist."
-	cp data/my_template/labelmap.prototxt data/${script_folder_name}
+    rm $FILE
 fi
-# Need to modify the labelmap.prototxt before running the rest of the scripts
+touch $FILE
+
+# Write backgound class
+echo "item {
+  name: \"none_of_the_above\"
+  label: 0
+  display_name: \"background\"
+}" >> $FILE
+# Write the rest of classes
+ind_class=0
+IFS=',' read -ra ADDR <<< "$CLASSES"
+for i in "${ADDR[@]}"; do
+	ind_class=$((ind_class+1))
+	echo "$i"
+	echo "item {
+  name: \"$i\"
+  label: $ind_class
+  display_name: \"$i\"
+}" >> $FILE
+done
+
 # ===============================================================
 
-# [3] Some processing to prepare for the rest of the scripts
+# [1b] Some processing to prepare for the rest of the scripts
 data_root_dir=`dirname "$DATA_DIR"`   #data_root_dir=$HOME/dixu/Datasets/QRData
 dataset_name=`basename "$DATA_DIR"`   #dataset_name="augmented"
 echo ''
@@ -23,33 +40,33 @@ echo 'data_root_dir:' $data_root_dir
 echo 'dataset_name:' $dataset_name
 
 
-# [4] run data_partition.sh
+# [1c] run data_partition.sh
 echo 'Running data_partition ...'
 ./data/my_template/data_partition.sh ${DATA_DIR} ${TEST_SET_PERCENTAGE}
 
 
-# [5] run create_list.sh
+# [1d] run create_list.sh
 echo 'Running create_list ...'
 ./data/my_template/create_list.sh $data_root_dir $dataset_name $IMAGE_FORMAT $script_folder_name
 
 
-# [6] run create_data.sh
+# [1e] run create_data.sh
 echo 'Running create_list ...'
 ./data/my_template/create_data.sh ${data_root_dir} ${dataset_name} ${script_folder_name}
 
 
-## TRAIN 
+## [2] TRAIN 
 
 
-# Use gen_model.sh to generate your own prototxt files for training and deployment
+# [2a] Use gen_model.sh to generate your own prototxt files for training and deployment
 cd MobileNet-SSD
 ./gen_model.sh $CLASSNUM 
 
-# Create a 'qr' folder
+# [2b] Create a 'qr' folder
 ProjectFolder="../../"${script_folder_name} #"caffe_ssd/qr"
 mkdir ${ProjectFolder}
 
-# Copy files to the 'ProjectFolder' 
+# [2c] Copy files to the 'ProjectFolder' 
 cp example/* ${ProjectFolder}
 # cp mobilenet_iter_73000.caffemodel ${ProjectFolder} 
 cp *.prototxt ${ProjectFolder}
@@ -57,10 +74,11 @@ cp train.sh ${ProjectFolder}
 cp test.sh ${ProjectFolder}
 
 
+# [2d] Create symlinks to lmdb data:
 cd ${ProjectFolder}
-# Create symlinks to lmdb data:
 ln -s ${DATA_DIR}/lmdb/trainval_lmdb trainval_lmdb
 ln -s ${DATA_DIR}/lmdb/test_lmdb test_lmdb
 
 
+# [2e] Train
 ./train.sh
