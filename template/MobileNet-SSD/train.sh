28fa5491282d4e9858ecb93215fd1db87308a9d3
Xu, Di <Di.Xu@flir.com>
2020-08-07 15:39:20 -0700

    replaced a hardcoded absolute path with a relative path

--- template/MobileNet-SSD/train.sh
+++ template/MobileNet-SSD/train.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #CAFFE_ROOT="$HOME/dixu/Projects/caffe_root"
-CAFFE_ROOT="$HOME/dixu/Projects/caffe_ssd/template/caffe_root"
+#CAFFE_ROOT="$HOME/dixu/Projects/caffe_ssd/template/caffe_root"
+CAFFE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"../template/caffe_root && pwd)"
+
 
 if ! test -f MobileNetSSD_train.prototxt ;then
 	echo "error: example/MobileNetSSD_train.prototxt does not exist."
